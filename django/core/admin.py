46f836b07157888913e577e0954134e07dc9cccd
Blessed <juankarma020@gmail.com>
2019-07-14 00:01:40 -0500

    add admin.site.register Person Role

--- django/core/admin.py
+++ django/core/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 
 #Agregando nuestro modelo Movie al ADMIN  de django, accesible desde el browser.
-from core.models import Movie
+from core.models import Movie, Person, Role
 
 
 # Register your models here.
 admin.site.register(Movie)
+admin.site.register(Person)
+admin.site.register(Role)
 
 
