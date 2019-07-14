60f222f255fa3c740f22ae4e767aabb72bf1fe3a
Blessed <juankarma020@gmail.com>
2019-07-13 23:27:29 -0500

    Add model Person and relationship | ForeignKey ManyToManyField and class Role

--- django/core/models.py
+++ django/core/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 
 # Create your models here.
-
+"""
+NOTA:
+	Acceso a todos los registros ->  Movie.objects.all() | dir(Sleuth) all methods | dir(Movie.objects) | dir(Movie)
+	Movie.NOT_RATED -> 0 |  test.rating -> 0-  mismo resultado
+	atributo ordering de clase Meta  Equivale a ORDER BY year DESC, title;
+	OJO: El orden de las clases importa.
+"""
 class Movie(models.Model):
+    """
+        Relaciones  Movie to Person
+	ForeingKey - director | 
+	ManyToManyField writers | 
+	ManyToManyField actors - agrega nueva clase Role con ForeingKeys de Movie y Person clases
+    """
+
     NOT_RATED = 0
     RATED_G = 1
     RATED_PG = 2
@@ -22,31 +35,40 @@ class Movie(models.Model):
     rating = models.IntegerField(choices=RATINGS, default=NOT_RATED)
     runtime = models.PositiveIntegerField()
     website = models.URLField(blank=True)
-    
+    #Relaciones
+    director = models.ForeignKey(to='Person', on_delete = models.SET_NULL, related_name = 'directed', null = True, blank = True)
+    writers = models.ManyToManyField(to = 'Person', related_name = 'writing_credits', blank = True)
+    actors = models.ManyToManyField(to = 'Person', through = 'Role', related_name = 'acting_credits', blank = True) #Solo actors agrega nueva Clase Role ?
+   
     class Meta:
-        ordering = ('-year', 'title') # Equivale a ORDER BY year DESC, title
+        ordering = ('-year', 'title') 
 
     def __str__(self):
-        return '{} ({})'.format(self.title, self.year) #Access into shell Movie.objects.all() | dir(Sleuth) all methods | dir(Movie.objects) | dir(Movie)
+        return '{} ({})'.format(self.title, self.year)
 
-							#Movie.NOT_RATED -> 0 |  test.rating -> 0 
- 
-#Use python Shell (python manage.py shell - python manage.py dbshell)to Add records to DB. 
-'''
-from core.models import Movie
-sleuth =  Movie.objects.create(
-    title="Sleuth",
-    plot="An snobbish writer who loves games"
-    " invites his wife\'s lover for a battle of wits.", 
-    year="1972",
-    runtime="138",
-)
+class Person(models.Model):
+    first_name = models.CharField(max_length=140)
+    last_name = models.CharField(max_length=140)
+    born = models.DateField()
+    died = models.DateField(null="True", blank="True")
 
-The_Italian_Job =  Movie.objects.create(
-    title="The Italian Job",
-    plot="A comic caper run by a cockney criminal"
-    "centres on hinges on a trafic jam", 
-    year="1969",
-    runtime="99",
-)
-'''
+    class Meta:
+        ordering = ("last_name", "first_name")
+
+    def __str__(self):
+        if self.died:
+            return "{}, {} ({}-{})".format(self.last_name, self.first_name, self.born, self.died)
+
+        return "{}, {} ({})".format(self.last_name, self.first_name, self.born) 
+
+
+class Role(models.Model):
+    movie = models.ForeignKey(Movie, on_delete = models.DO_NOTHING)
+    person = models.ForeignKey(Person, on_delete = models.DO_NOTHING)
+    name = models.CharField(max_length = 140)
+
+    def __str__(self):
+        return "{} {} {}".format(self.movie_id, self.person_id, self.name)
+
+    class Meta:
+        unique_together = ('movie', 'person', 'name')
