0472cddf3e6433959bbe704cad0d1c628c565448
Blessed <juankarma020@gmail.com>
2019-09-12 21:06:09 -0500

    update core/views

--- django/core/views.py
+++ django/core/views.py
@@ -3,8 +3,6 @@
 #Vistas basadas en clases ListView.
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
-from core.models import Movie, Person
-
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import redirect
@@ -25,7 +23,7 @@ class MovieDetail(DetailView): #Una instancia del modelo  a la vez
         if self.request.user.is_authenticated:
             vote = Vote.objects.get_vote_or_unsaved_blank_vote(movie = self.object, user = self.request.user)
             if vote.id:
-                vote_form_url = reverse("core:Update", kwargs = {"movie_id": vote.movie.id, "pk":vote.id})
+                vote_form_url = reverse("core:Update", kwargs = {"movie_id": vote.movie.id, "pk": vote.id})
             else:
                 vote_form_url = (reverse("core:CreateVote", kwargs = {"movie_id":self.object.id}))
             vote_form = VoteForm(instance = vote)
@@ -68,7 +66,7 @@ class UpdateVote(LoginRequiredMixin, UpdateView):
         return vote
 
     def get_success_url(self):
-        movie_id = self.object,movie.id
+        movie_id = self.object.movie.id
         return reverse("core:MovieDetail", kwargs={"pk": movie_id})
 
     def render_to_response(self, context, **response_kwargs):
